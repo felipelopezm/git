4e93f1adfc366e34dfb9120a383129a34e85453e
felipelopezm <62429174+felipelopezm@users.noreply.github.com>
2020-04-16 22:25:17 -0500

    Initial commit

---
+++ CarritosArduino/Assets/ScoringSystem.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoringSystem : MonoBehaviour
+{
+
+    public GameObject scoreText;
+    public int theScore;
+    public AudioSource collectSound;
+
+    void OnTriggerEnter(Collider other)
+    {
+
+       
+        theScore += 10;
+        scoreText.GetComponent<Text>().text = "ESTRELLAS: " + theScore;
+        Destroy(gameObject);
+
+
+    }
+
+}
---
+++ CarritosArduino/Assets/scripttamano.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+using System.IO.Ports; //Library to read our ardunio data
+using System.Collections;//to use Ienumarator in our code
+
+public class scripttamano : MonoBehaviour
+{
+    SerialPort seri = new SerialPort("COM3", 9600);//define our port
+    // Use this for initialization
+    void Start()
+    {
+
+        seri.Open(); // iba en el otro
+
+        seri.ReadTimeout = 1;
+
+
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+    if (seri.IsOpen) // iba en el otro
+            {
+                try
+                {
+
+
+                    if (seri.ReadByte() == 1)
+                    {
+                        transform.localScale = new Vector3(2.0f, 2.0f, 2.0f);
+}
+                    else if (seri.ReadByte() == 2)
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y + 0.5f, transform.position.z);
+transform.localScale = new Vector3(5.25f, 5.25f, 5.25f);
+                    }
+                    else if (seri.ReadByte() == 3)
+                    {
+                        transform.position = new Vector3(transform.position.x, transform.position.y + 1.0f, transform.position.z);
+transform.localScale = new Vector3(10.5f, 10.5f, 10.5f);
+                    }
+                }
+                catch (System.Exception) { }
+            }
+
+    }
+}
